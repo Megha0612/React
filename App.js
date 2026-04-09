11e1558a0dda5f54277c711a5a10befa95a4e0ee
Megha <megha@Meghas-MacBook-Air.local>
2026-04-09 09:18:10 +0530

    Episode-2

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-// const heading = React.createElement("h1", { id: "heading" }, "This is React!");
-
-
-
-// {/* <div class="parent">
-//     <div class="child">
-//         <h1>I am h1 tag.</h1>
-// <h2>I am h2 tag</h2>
-//     </div>
-// </div> */}
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     "div",
@@ -16,10 +7,12 @@ const parent = React.createElement(
     React.createElement(
         "div",
         { id: "child" },
-        [React.createElement("h1", {}, "This is h1 tag."),
-        React.createElement("h2", {}, "This is h2 tag."),
-        ])
-)
+        [
+            React.createElement("h1", { key: "h1" }, "This is h1 tag."),
+            React.createElement("h2", { key: "h2" }, "This is h2 tag.")
+        ]
+    )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
